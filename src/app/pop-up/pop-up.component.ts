d0e99ec339f804d612c847b9a604458ce07a97e6
Gokul <gokulchandan18@gmail.com>
2021-12-21 16:41:00 +0530

    commit

--- src/app/pop-up/pop-up.component.ts
+++ src/app/pop-up/pop-up.component.ts
@@ -8,17 +8,28 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./pop-up.component.css']
 })
 export class PopUpComponent  {
+listData:any;
 
   constructor(private formBuilder:FormBuilder){}
 
-  profileForm = this.formBuilder.group({
+   profileForm = this.formBuilder.group({
+    
     firstName:[''],
     lastName:[''],
     gender:['']
+    
   });
  
   saveForm(){
+    
     console.log('Form data is ', this.profileForm.value);
   }
- 
+ public addItem():void{
+   this.listData.push(this.profileForm.value);
+   this.profileForm.reset();
+ }
+
+ ngOnInit(){
+   
+ }
  }
