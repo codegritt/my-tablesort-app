3d057d956cfb9c5a00600333c9b1199a88428994
Gokul <gokulchandan18@gmail.com>
2021-12-21 16:21:49 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ import { PopUpComponent } from './pop-up/pop-up.component';
 export class AppComponent implements OnInit {
   service: any;
   profileForm: any;
-  listData: any;
+ 
   title(title: any) {
     throw new Error('Method not implemented.');
   }
